cbccad97e8263daf4a9f07748e40ab1a3be6155e
mubaarak2002 <mubaarak.k@outlook.com>
2023-03-06 12:48:50 +0000

    hashtable for variable/register pairs

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -2,14 +2,53 @@
 #include <iostream>
 #include <unistd.h>
 #include <iomanip>
+#include <unordered_map>
 
 #include "ast.hpp"
 
 #include "cli.h"
 
+typedef std::unordered_map<std::string,std::string> histogram_type;
+
 void compile(std::ostream &w)
 {
     const Expression *ast=parseAST();
+
+    histogram_type registers = {
+        {"zero", "."},
+        {"ra", "."},
+        {"sp", "."},
+        {"gp", "."},
+        {"tp", "."},
+        {"t0", "."},
+        {"t1", "."},
+        {"t2", "."},
+        {"s0", "."},
+        {"s1", "."},
+        {"a0", "."},
+        {"a1", "."},
+        {"a2", "."},
+        {"a3", "."},
+        {"a4", "."},
+        {"a5", "."},
+        {"a6", "."},
+        {"a7", "."},
+        {"s2", "."},
+        {"s3", "."},
+        {"s4", "."},
+        {"s5", "."},
+        {"s6", "."},
+        {"s7", "."},
+        {"s8", "."},
+        {"s9", "."},
+        {"s10", "."},
+        {"s11", "."},
+        {"t3", "."},
+        {"t4", "."},
+        {"t5", "."},
+        {"t6", "."}
+    };
+
 
     // evaluate it
     std::map<std::string,double> bindings;
@@ -25,6 +64,10 @@ void compile(std::ostream &w)
     w << "add   a0, zero, t0" << std::endl;
     w << "ret" << std::endl;
 
+    w << registers["a0"] << std::endl;
+    registers["a0"] = "var";
+    w << registers["a0"] << std::endl;
+
 }
 
 // TODO: uncomment the below if you're using Flex/Bison.
@@ -32,6 +75,7 @@ extern FILE *yyin;
 
 int main(int argc, char **argv)
 {
+
     // Parse CLI arguments, to fetch the values of the source and output files.
     std::string sourcePath = "";
     std::string outputPath = "";
